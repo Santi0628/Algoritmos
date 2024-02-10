e5e489fe7f61509e640f6664f60ac67d2ede48d6
Santi0628 <santiago.gc0628@gmail.com>
2024-02-09 21:28:55 -0500

    Cambiando de PC.

--- iniciando/Exercise2.java
+++ iniciando/Exercise2.java
@@ -10,16 +10,27 @@ public class Exercise2 {
         // boolean resultado = busqueda(arreglo, dato);
         // System.out.println(resultado);
 
-        double[][] matriz = {
+        /* double[][] matriz = {
                 {1,  2,  3,  4,  5},
                 {2,  7,  8,  9,  10},
                 {3,  8,  13, 14, 16},
                 {4,  9,  14, 20, 21},
                 {5,  10, 16, 21, 26}
-        };
+        }; */
 
-        boolean resultado = simetrica(matriz);
-        System.out.println(resultado);
+        // boolean resultado = simetrica(matriz);
+        // System.out.println(resultado);
+
+        double [] [] matriz = new double[5][5];
+        double [][] matrizLlena = fullMatrix(matriz.length, matriz[0].length, matriz, 0, 0);
+        for (double[] doubles : matrizLlena) {
+            System.out.print("|");
+            for (int y = 0; y < doubles.length; y++) {
+                System.out.print(doubles[y]);
+                if (y != doubles.length - 1) System.out.print("\t");
+            }
+            System.out.println("|");
+        }
     }
 
     static boolean busqueda(int[] arreglo, int dato)
@@ -49,6 +60,26 @@ public class Exercise2 {
             }
         }
         return auxiliar;
+    }
+
+    public static double [][] fullMatrix(int fila, int columna, double [][] matriz, int countRow, int countColumn){
+        if(countColumn < columna && countRow < fila && matriz[4][1] == 0){
+           matriz[countRow] [0] = 4;
+           countRow += 1;
+           return fullMatrix(fila, columna, matriz, countRow, countColumn);
+        }else {
+            countRow = 0;
+        }
+
+        if(matriz[0][4] == 0){
+            matriz[0][countColumn] = 4;
+            countColumn += 1;
+            return fullMatrix(fila, columna, matriz, countRow, countColumn);
+        }
+
+
+
+        return matriz;
     }
 
 }
